6ca5fd2a7902edfdc2fe8cb7b2921fa6db406a80
Patrik Hedman <patrik.hedman@gmail.com>
2010-08-20 21:20:09 +0200

    described dependencies

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject funkyweb "0.1.0"
+  :description "A ring based web framework for clojure"
+  :dependencies     [[ org.clojure/clojure         "1.2.0"          ]
+                     [ org.clojure/clojure-contrib "1.2.0"          ]
+                     [ ring/ring-core              "0.2.5"          ]
+                     [ ring/ring-jetty-adapter     "0.2.5"          ]
+                     [ clout                       "0.2.0"          ]]
+  :dev-dependencies [[ ring/ring-devel             "0.2.5"          ]
+                     [ swank-clojure               "1.3.0-SNAPSHOT" ]])
