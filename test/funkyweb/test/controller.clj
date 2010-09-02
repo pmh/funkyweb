78cfb26b6c2bc5804deef4bf479e1fd81f0170a0
Patrik Hedman <patrik.hedman@gmail.com>
2010-09-02 11:55:58 +0200

    added controller tests

--- test/funkyweb/test/controller.clj
+++ test/funkyweb/test/controller.clj
@@ -1,3 +1,42 @@
 (ns funkyweb.test.controller
-  (:use [funkyweb.controller] :reload-all)
+  (:use [funkyweb.controller]        :reload-all)
   (:use [clojure.test]))
+
+(binding [*ns* (create-ns 'myapp.controllers.dashboard)]
+
+  (defn execute [method uri]
+    (funkyweb.controller.router/execute {:request-method method :uri uri}))
+
+
+  (GET no-params [] "dashboard#no-params")
+  
+  (deftest test-no-params-builds-a-uri
+    (= (no-params) "/dashboard/no-params"))
+  (deftest test-no-params-action-method-returns-body
+    (= (execute :get "/dashboard/no-params") "dashboard#no-params"))
+
+
+  (GET say-hello [name] (str "Hello, " name "!"))
+
+  (deftest test-say-hello-builds-a-uri
+    (= (say-hello "foo") "/dashboard/say-hello/foo"))
+  (deftest test-say-hello-action-method-returns-body
+    (= (execute :get "/dashboard/say-hello/foo") "Hello, foo!"))
+
+
+  (GET hello-foo []
+       (str "<a href='" (say-hello "foo") "'>Say hello to foo" "</a>"))
+
+  (deftest test-hello-foo-builds-a-uri
+    (= (hello-foo) "/dashboard/hello-foo"))
+  (deftest test-hello-foo-action-method-returns-body
+    (= (execute :get "/dashboard/hello-foo")
+       "<a href='/dashboard/say-hello/foo'>Say hello to foo</a>"))
+
+
+  (GET add [:int a :int b] (str a " + " b " = " (+ a b)))
+
+  (deftest test-add-builds-a-uri
+    (= (add 1 2) "/dashboard/say-hello/1/2"))
+  (deftest test-say-hello-action-method-returns-body
+    (= (execute :get "/dashboard/add/1/2") "1 + 2 = 3")))
