fe34267321c7a3f0b0a04b45b23891e8768c34d7
Patrik Hedman <patrik.hedman@gmail.com>
2010-09-04 17:44:14 +0200

    refactored the GET macro a bit

--- src/funkyweb/controller.clj
+++ src/funkyweb/controller.clj
@@ -1,18 +1,25 @@
 (ns funkyweb.controller
   (:use funkyweb.controller.router)
-  (:use (ring.middleware reload stacktrace)))
+  (:use (ring.middleware stacktrace)))
 
-(defmacro GET [name args & forms]
+(defmacro construct-url-helper [name args]
   `(let [ns# *ns*]
      (defn ~name [~@(strip-type-hints args)]
        (binding [*ns* ns#]
          (let [& "&"]
            (build-path '~name
                        (flatten
-                        (strip-type-hints (filter #(not (= "&" %)) ~args)))))))
-     (add-route :get '~name (replace-varargs-with-star '~args)
-                (fn [~@(strip-type-hints args)]
-                  ~@forms))))
+                        (strip-type-hints (filter #(not (= "&" %)) ~args)))))))))
+
+(defmacro construct-route [http-verb name args forms]
+  `(add-route ~http-verb '~name (replace-varargs-with-star '~args)
+              (fn [~@(strip-type-hints args)]
+                ~@forms)))
+
+(defmacro GET [name args & forms]
+  `(do
+    (construct-url-helper ~name ~args)
+    (construct-route :get ~name ~args ~forms)))
 
 (defn generate-response [status body]
   {:status  status
