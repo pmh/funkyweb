b4cdadf98d18e211a50d47096844ba68b2563ce5
Patrik Hedman <patrik.hedman@gmail.com>
2010-09-20 22:15:00 +0200

    added cookies aliases

--- src/funkyweb/helpers.clj
+++ src/funkyweb/helpers.clj
@@ -1,7 +1,12 @@
 (ns funkyweb.helpers
   (:use     [clojure.contrib.def :only (defalias)])
-  (:require [funkyweb.helpers.session :as session]))
+  (:require [funkyweb.helpers.session :as session]
+            [funkyweb.helpers.cookies :as cookies]))
 
 (defalias session-set   session/session-set)
 (defalias session-get   session/session-get)
 (defalias alter-session session/alter-session)
+
+(defalias cookies-set   cookies/cookies-set)
+(defalias cookies-get   cookies/cookies-get)
+(defalias alter-cookies cookies/alter-cookies)
