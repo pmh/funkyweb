a38ddda1614c84d4d8f727835758b11b9d85b951
Patrik Hedman <patrik.hedman@gmail.com>
2010-09-20 22:16:20 +0200

    added request alias

--- src/funkyweb/helpers.clj
+++ src/funkyweb/helpers.clj
@@ -1,7 +1,8 @@
 (ns funkyweb.helpers
   (:use     [clojure.contrib.def :only (defalias)])
   (:require [funkyweb.helpers.session :as session]
-            [funkyweb.helpers.cookies :as cookies]))
+            [funkyweb.helpers.cookies :as cookies]
+            [funkyweb.helpers.request :as request]))
 
 (defalias session-set   session/session-set)
 (defalias session-get   session/session-get)
@@ -10,3 +11,5 @@
 (defalias cookies-set   cookies/cookies-set)
 (defalias cookies-get   cookies/cookies-get)
 (defalias alter-cookies cookies/alter-cookies)
+
+(defalias request-get   request/request-get)
