9bd3bdd366ae207133bc59f2e78135c00add306b
Patrik Hedman <patrik.hedman@gmail.com>
2010-09-21 14:12:35 +0200

    small refactoring

--- src/funkyweb/controller/helpers.clj
+++ src/funkyweb/controller/helpers.clj
@@ -6,15 +6,14 @@
   (string/replace (str "/" name) #"->" "/"))
 
 (defmacro construct-url-helper [name args]
-  `(let [ns# *ns*
-         controller-name# *controller-name*]
-     (defn ~name [~@(strip-type-hints args)]
-       (binding [*ns* ns#
-                 *controller-name* controller-name#]
-         (let [& "&"]
-           (build-path '~name
-                       (flatten
-                        (strip-type-hints (filter #(not (= "&" %)) ~args)))))))))
+  `(let [controller-name# *controller-name*]
+     (def ~name
+          (fn [~@(strip-type-hints args)]
+            (binding [*controller-name* controller-name#]
+              (let [& "&"]
+                (build-path '~name
+                            (flatten
+                             (strip-type-hints (filter #(not (= "&" %)) ~args))))))))))
 
 (defmacro construct-route [http-verb name args forms]
   `(add-route ~http-verb '~name (replace-varargs-with-star '~args)
