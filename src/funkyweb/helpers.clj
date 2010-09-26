fad9fa765458b1e529b6f9d48f73670ef81f1f08
Patrik Hedman <patrik.hedman@gmail.com>
2010-09-26 17:32:46 +0200

    reorganized helpers and defined aliases for response and flash

--- src/funkyweb/helpers.clj
+++ src/funkyweb/helpers.clj
@@ -10,15 +10,15 @@
 (defalias cookies-set   cookies/cookies-set)
 (defalias cookies-get   cookies/cookies-get)
 
-(defalias request-get   request/request-get)
-(defalias query-string  request/query-string)
-(defalias qs            request/query-string)
+(defalias flash-get     flash/flash-get)
+(defalias flash-set     flash/flash-set)
 
 (defn redirect-to [f & args]
   (let [uri (if (string? f) f (apply f args))]
-    {:status 301
-     :headers {"Location" uri}
-     :body (str "You are being redirected to: " uri)}))
+    (response/response merge
+                       {:status  301
+                        :headers {"Location" uri}
+                        :body    (str "You are being redirected to: " uri)})))
 
 (defn respond-with [& {:keys [html xml json]}]
   (condp = (request-get :content-type)
