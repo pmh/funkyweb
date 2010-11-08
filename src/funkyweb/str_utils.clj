8efef7fb49025a4889b9a9bdd62621967b9b854a
Patrik Hedman <pmh@RapidStream.(none)>
2010-11-08 01:48:56 +0100

    whiped everything, starting over with a clean slate.

--- src/funkyweb/str_utils.clj
+++
@@ -1,5 +0,0 @@
-(ns funkyweb.str-utils)
-
-(defn ends-with? [match s]
-  (= (subs s (- (count s) (count match)))
-     match))
--- test/funkyweb/test/str_utils.clj
+++
@@ -1,9 +0,0 @@
-(ns funkyweb.test.str-utils
-  (:use [funkyweb.str-utils] :reload-all)
-  (:use [clojure.test]))
-
-(deftest test-ends-with?
-  (are [match s expected]
-       (= (ends-with? match s) expected)
-       "/" "foo/" true
-       "/" "foo"  false))
