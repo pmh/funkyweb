d307434dcbaa72559d9e0dead2b02f94fa650555
Patrik Hedman <patrik.hedman@gmail.com>
2010-12-27 09:42:17 -0500

    handle IllegalArgumentException as a 404 and handle Error the same way as Exception

--- src/funkyweb/controller.clj
+++ src/funkyweb/controller.clj
@@ -24,16 +24,25 @@
     (assoc req :request-method (keyword (.toLowerCase method)))
     req))
 
+(defn handle-exception [ex & {:keys [with-status]}]
+  (let [exception (symbol (.getName (.getClass ex)))]
+    (if-let [error-handler (first (select exception @error-handlers))]
+      (render ((eval (get error-handler exception)) request response) request @response)
+      (render with-status request @response))))
+
 (defn handler [req]
   (binding [request  (fix-request-method req)
             response (atom response)]
     (try
-      (render ((find-resource-for req)) request @response)
+      (if-let [resource (find-resource-for req)]
+        (render (resource) request @response)
+        (render 404 request @response))
+      (catch IllegalArgumentException ex
+        (handle-exception ex :with-status 404))
       (catch Exception ex
-        (let [exception (symbol (.getName (.getClass ex)))]
-          (if-let [error-handler (first (select exception @error-handlers))]
-            (render ((eval (get error-handler exception)) request response) request @response)
-            (render 404 request @response)))))))
+        (handle-exception ex :with-status 500))
+      (catch Error ex
+        (handle-exception ex :with-status 500)))))
 
 (def ^{:private true} app
   (-> #'handler
